0e5404b81c7565dee37e90f9dfc0e3056a06120a
jooyg62 <jooyg62@naver.com>
2019-06-12 13:11:07 +0900

    init commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+# hello.py
+
+
+def add(m, n):
+    s = m
+    s += n
+    return s
+
+
+def mymax(m, n):
+    if m > n:
+        return m
+    else:
+        return n
+
+
+a = 1
+b = 1
+a = 2
+
+if a > 1:
+    print('big')
+    print('big')
+
+    for i in range(1, 10):
+        pass
+
+    for i in range(1, 10):
+        print('--->', i, i, i)
+
+print('hello world')
